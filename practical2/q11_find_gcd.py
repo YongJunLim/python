027cf7a2a1cb2b2433210b9bcb4a420982fa25a6
DHSYongJun <lim.yongjun@dhs.sg>
2018-02-05 21:59:50 +0800

    Updated

--- practical2/q11_find_gcd.py
+++ practical2/q11_find_gcd.py
@@ -1,9 +1,11 @@
 n1 = int(input("1st integer: "))
 n2 = int(input("2nd integer: "))
+d = min(n1,n2)
 
-def gcd(m, n):
-    while n:
-        m, n = n, m%n
-    return m
+def gcd(d,n1,n2):
+    if n1 % d == 0 and n2 % d == 0:
+        return d
+    else:
+        return gcd(d-1,n1,n2)
 
-print(gcd(n1,n2))
+print(gcd(d,n1,n2))
